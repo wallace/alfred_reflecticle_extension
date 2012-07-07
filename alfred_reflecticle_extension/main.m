6d970664d24c26aeb54cf48a5d8131ef786f1473
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2012-07-07 01:30:19 -0400

    Initial Commit

---
+++ alfred_reflecticle_extension/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  alfred_reflecticle_extension
+//
+//  Created by Jonathan Wallace on 7/7/12.
+//  Copyright (c) 2012 MH Solutions LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
